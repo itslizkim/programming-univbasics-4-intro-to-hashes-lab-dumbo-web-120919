2a718f8abd622c8b7235b217c8d570af711db002
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-04 06:44:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,10 +24,5 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = {},'hello'
- if hash[key]
-   hash[key] = 1
- else
-   hash[key] += 1
- end
+  hash[key] ? hash[key] += 1 : hash[key] = 1
 end
