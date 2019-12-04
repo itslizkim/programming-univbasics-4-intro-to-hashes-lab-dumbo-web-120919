916e1ca50858ce6e384d2ca9c27141e5709e8916
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-04 06:42:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- hash[key] ? hash[key]+= 1 : hash[key] = 1
- hash
+  hash
+
+ if hash[key]
+   hash[key] = 1
+ else
+   hash[key] += 1
+ end
 end
